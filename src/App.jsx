24ef6a37c5681c40975f9db13f3ce6b483946a54
iiSurf <87113036+iiSurf@users.noreply.github.com>
2024-07-13 21:51:09 -0700

    We have a working text box inside of form

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { react, useReducer } from 'react';
+import { react, useReducer, useState } from 'react';
 import './App.css';
 // npm install react-router-dom
 
@@ -11,11 +11,13 @@ function reducer(state, action) {
 };
 
 function App() {
-  const [todos, dispatch] = useReducer(reducer, [])
+  const [todos, dispatch] = useReducer(reducer, []);
+  const [name, setName] = useState('');
+
   return (
     <>
       <form>
-
+        <input type='text' value={name} onChange={e => setName(e.target.value)} />
       </form>
     </>
   )
