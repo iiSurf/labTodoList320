bffe4df3c1183a1e21c14bf478c62eb3e3358a1e
iiSurf <87113036+iiSurf@users.noreply.github.com>
2024-07-14 17:47:14 -0700

    creating, deleting, and toggling to do list items can now be done

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import { react, useReducer, useState } from 'react';
-import Todo from './Todo';
-import './App.css';
+import { react, useReducer, useState } from "react";
+import Todo from "./Todo";
+import "./App.css";
 // npm install react-router-dom
 
-const ACTIONS = {
-  ADD_TODO : 'add-todo',
-  TOGGLE_TODO: 'toggle-todo'
+export const ACTIONS = {
+  ADD_TODO: "add-todo",
+  TOGGLE_TODO: "toggle-todo",
+  DELETE_TODO: "delete-todo",
 };
 
 function reducer(todos, action) {
@@ -13,37 +14,47 @@ function reducer(todos, action) {
     case ACTIONS.ADD_TODO:
       return [...todos, newTodo(action.payload.name)];
     case ACTIONS.TOGGLE_TODO:
-      break;
-  
+      return todos.map((todo) => {
+        if (todo.id === action.payload.id) {
+          return { ...todo, complete: !todo.complete };
+        }
+        return todo;
+      });
+    case ACTIONS.DELETE_TODO:
+      return todos.filter(todo => todo.id != action.payload.id)
     default:
-      break;
+      return todos;
   }
-};
+}
 
 function newTodo(name) {
-  return {id: Date.now(), name: name, complete: false}
+  return { id: Date.now(), name: name, complete: false };
 }
 
 function App() {
   const [todos, dispatch] = useReducer(reducer, []);
-  const [name, setName] = useState('');
+  const [name, setName] = useState("");
 
   function handleSubmit(e) {
     e.preventDefault();
-    dispatch({type: ACTIONS.ADD_TODO, payload: {name: name}});
-    setName('');
-  };
+    dispatch({ type: ACTIONS.ADD_TODO, payload: { name: name } });
+    setName("");
+  }
 
   return (
     <>
       <form onSubmit={handleSubmit}>
-        <input type='text' value={name} onChange={e => setName(e.target.value)} />
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
       </form>
-      {todos.map(todo => {
-        return <Todo key={todos.id} todo={todo} />
+      {todos.map((todo) => {
+        return <Todo key={todos.id} todo={todo} dispatch={dispatch} />;
       })}
     </>
-  )
-};
+  );
+}
 
 export default App;
--- src/Todo.jsx
+++ src/Todo.jsx
@@ -1,13 +1,14 @@
-import React from 'react'
+import React from 'react';
+import { ACTIONS } from "./App";
 
-function Todo({ todo }) {
+function Todo({ todo, dispatch }) {
   return (
     <div>
         <span style={{color: todo.complete ? '#AAA' : '#000'}}>
             {todo.name}
         </span>
-        <button>Toggle</button>
-        <button>Delete</button>
+        <button onClick={() => dispatch({ type: ACTIONS.TOGGLE_TODO, payload: {id: todo.id }})}>Toggle</button>
+        <button onClick={() => dispatch({ type: ACTIONS.DELETE_TODO, payload: {id: todo.id }})}>Delete</button>
     </div>
   )
 }
